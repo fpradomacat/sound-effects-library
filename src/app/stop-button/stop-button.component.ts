0ad21f9b5614cb35be16868f2562e87e5781e8bf
Federico Prado Macat <fpradomacat@gmail.com>
2022-05-24 22:55:23 +0200

    migrated stop button component

--- src/app/stop-button/stop-button.component.ts
+++ src/app/stop-button/stop-button.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { SoundPlayerService } from "../sound-player.service";
 
 @Component({
   selector: 'app-stop-button',
   templateUrl: './stop-button.component.html',
   styleUrls: ['./stop-button.component.scss']
 })
-export class StopButtonComponent implements OnInit {
+export class StopButtonComponent {
 
-  constructor() { }
+  isStopButtonActive = false;
 
-  ngOnInit(): void {
+  constructor(private soundPlayerService: SoundPlayerService) {
+  }
+
+  public stopCurrentSound(): void {
+    this.soundPlayerService.stopCurrentSound();
+  }
+
+  @HostListener('window:keydown.shift')
+  handleShiftKeydown() {
+    this.isStopButtonActive = true;
+  }
+
+  @HostListener('window:keyup.shift')
+  handleShiftKeyup() {
+    this.isStopButtonActive = false;
   }
 
 }
