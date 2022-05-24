ba62fe04cb286565175e120963b741614ee4d41e
Federico Prado Macat <fpradomacat@gmail.com>
2022-05-24 23:06:50 +0200

    migrated dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Section } from "../classes/Section";
+import { SoundPlayerService } from "../sound-player.service";
+import { SoundsLibraryService } from "../sounds-library.service";
+import { Hotkey } from "../classes/Hotkey";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public soundsLibraryService: SoundsLibraryService,
+              private soundPlayerService: SoundPlayerService) {
   }
 
+  @HostListener('window:keydown', ['$event'])
+  handleKeyPressed(event: KeyboardEvent) {
+    if (event.shiftKey) {
+      this.soundPlayerService.stopCurrentSound();
+    } else {
+      const hotkey: Hotkey = {key: event.key, isCtrlPressed: event.ctrlKey};
+      this.soundPlayerService.playSoundByHotkey(hotkey);
+    }
+  }
 }
